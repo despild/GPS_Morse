7306dff96566007d3b377d360d9e720dcd0e15aa
despild <despild@gmail.com>
2012-01-30 17:44:22 +0900

    Add DFRuino Sheild Code

--- GPS_DF_Morse/GPS_DF_Morse.ino
+++ GPS_DF_Morse/GPS_DF_Morse.ino
@@ -1,206 +1,286 @@
-#include <string.h>
 #include <Wire.h>
-int GPSAddress = 0x42;
 
-int GPSrxPin=0;
-int GPStxPin =1;
+#define BUFFER_LENGTH 10//Define the buffer length
+
 int tonePin= 8;
 int pttPin =11;
-int unitTime = 60; 
+int unitTime = 30; 
 int dotTime =unitTime;
 int dashTime =unitTime*3;
 int timeWord =unitTime*3;
 int timeLetter=unitTime*7;
 int TONE =700;
-String readLine ="";
-boolean stringComplete = false;
-String time="hhmmss";
-String la="ddmm.mmmm";
-String ns="a";
-String lo="dddmm.mmmm";
-String ew="a";
-String al="x.x";
-String sat="xx";
-String message="test";
 
-float fLa=0;
-float fLo=0;
-float fAl=0;
-int floatCount=0;
 
-boolean morseTime=true;
-int timeCounter=0;
+int GPSAddress = 0x42;//GPS I2C Address
 
-void rec_init(){
+double Datatransfer(char *data_buf,char num)//Data type converter：convert char type to float
+{                                           //*data_buf:char data array ;num:float length
+  double temp=0.0;
+  unsigned char i,j;
+
+  if(data_buf[0]=='-')//负数的情况
+  {
+    i=1;
+    //数组中的字符型数据转换成整数并累加
+    while(data_buf[i]!='.')
+      temp=temp*10+(data_buf[i++]-0x30);
+    for(j=0;j<num;j++)
+      temp=temp*10+(data_buf[++i]-0x30);
+    //将转换后的整数转换成浮点数
+    for(j=0;j<num;j++)
+      temp=temp/10;
+    //转换成负数
+    temp=0-temp;
+  }
+  else//正数情况
+  {
+    i=0;
+    while(data_buf[i]!='.')
+      temp=temp*10+(data_buf[i++]-0x30);
+    for(j=0;j<num;j++)
+      temp=temp*10+(data_buf[++i]-0x30);
+    for(j=0;j<num;j++)
+      temp=temp/10 ;
+  }
+  return temp;
+}
+void rec_init()//initial GPS
+{
   Wire.beginTransmission(GPSAddress);
-  Wire.write(0xff);
-  Wire.endTransmission();
- 
+  Wire.write(0xff);//发送数据所在的地址      
+  Wire.endTransmission(); 
+
   Wire.beginTransmission(GPSAddress);
-  Wire.requestFrom(GPSAddress,50);
-} 
-  
-  
-void setup(){
-  Wire.begin();
-  //pinMode(GPSrxPin,INPUT);
-  //pinMode(GPStxPin,OUTPUT);
+  Wire.requestFrom(GPSAddress,10);//要求从GPS器件读取10个字节
+}
+char ID()//接收语句的ID
+{
+  char i = 0;
+  char value[7]={
+    '$','G','P','G','G','A',','      };//要接收的GPS语句的ID内容
+  char buff[7]={
+    '0','0','0','0','0','0','0'      };
+
+  while(1)
+  {
+    rec_init();//接收数据初始化    
+    while(Wire.available())   
+    { 
+      buff[i] = Wire.read();//接收串口的数据  
+      if(buff[i]==value[i])//对比是否是正确的ID
+      {
+        i++;
+        if(i==7)
+        {
+          Wire.endTransmission();//结束接收
+          return 1;//接收完毕返回1
+        }
+      }
+      else
+        i=0;
+    }
+    Wire.endTransmission();//结束接收
+  }
+}
+void UTC()//获取时间信息
+{
+  char i = 0,flag=0;
+  char value[7]={
+    '$','G','P','G','G','A',','   };
+  char buff[7]={
+    '0','0','0','0','0','0','0'       };
+  char time[9]={
+    '0','0','0','0','0','0','0','0','0'    };//存放时间数据
+  double t=0.0;
+
+  while(1)
+  {
+    rec_init();    
+    while(Wire.available())   
+    { 
+      if(!flag)
+      { 
+        buff[i] = Wire.read();
+        if(buff[i]==value[i])
+        {
+          i++;
+          if(i==7)
+          {
+            i=0;
+            flag=1;
+          }
+        }
+        else
+          i=0;
+      }
+      else
+      {
+        time[i] = Wire.read();
+        i++;
+        if(i==9)
+        {
+          t=Datatransfer(time,2);//转换成浮点型数据
+          t=t+80000.00;//将时间转换成北京时间
+          Serial.println(t);//输出时间数据 
+          morseTalk(time,9);
+          Wire.endTransmission();
+          return;
+        }
+      }
+    }
+    Wire.endTransmission(); 
+  }
+}
+void rec_data(char *buff,char num1,char num2)//接收数据子函数
+{                                            //*buff：存放接收数据的数组；num1：逗号数目；num2：数组长度。
+  char i=0,count=0;
+
+  if(ID())
+  {
+    while(1)
+    {
+      rec_init();    
+      while(Wire.available())   
+      { 
+        buff[i] = Wire.read();
+        if(count!=num1)
+        {  
+          if(buff[i]==',')
+            count++;
+        }
+        else
+        {
+          i++;
+          if(i==num2)
+          {
+            Wire.endTransmission();
+            return;
+          }
+        }
+      }
+      Wire.endTransmission();
+    }
+  }
+}
+void latitude()//获取纬度信息
+{
+  char lat[10]={
+    '0','0','0','0','0','0','0','0','0','0' };//存放纬度数据
+  rec_data(lat,1 ,10);//接收纬度数据
+            morseTalk(lat,10);
+  Serial.println(Datatransfer(lat,5),5);//将纬度数据转换成浮点型数据并输出
+}
+void lat_dir()//获取纬度方向信息
+{
+  char dir[1]={'0'};//存放纬度方向数据
+  rec_data(dir,2,1);//接收纬度方向数据
+  morseTalk(dir,1);
+  Serial.println(dir[0]);//将纬度方向信息输出
+}
+void  longitude()//获取经度信息
+{
+  char lon[11]={
+    '0','0','0','0','0','0','0','0','0','0','0' };//存放经度数据
+  rec_data(lon,3,11);//接收经度数据
+            morseTalk(lon,11);
+  Serial.println(Datatransfer(lon,5),5);//将经度数据转换成浮点型数据并输出
+}
+void lon_dir()//获取经度方向信息
+{
+  char dir[1]={'0'};
+  rec_data(dir,4,1);
+  morseTalk(dir,1);
+  Serial.println(dir[0]);
+}
+void altitude()//获取海拔信息
+{
+  char i=0,count=0;
+  char alt[8]={
+    '0','0','0','0','0','0','0','0' };
+
+  if(ID())
+  {
+    while(1)
+    {
+      rec_init();    
+      while(Wire.available())   
+      { 
+        alt[i] = Wire.read();
+        if(count!=8)
+        {  
+          if(alt[i]==',')
+            count++;
+        }
+        else
+        {
+          if(alt[i]==',')
+          {
+                      morseTalk(alt,8);
+            Serial.println(Datatransfer(alt,1),1);
+            Wire.endTransmission();
+            return;
+          }
+          else
+            i++;
+        }
+      }
+      Wire.endTransmission();
+    }
+  }
+}
+void setup()
+{
   pinMode(pttPin,OUTPUT);
   pinMode(tonePin,OUTPUT);
-  Serial.begin(9600);
-  readLine.reserve(200);
-  timeCounter=(int)millis();
-
-  delay(1000);
-  /*
-  //test
-  digitalWrite(pttPin,HIGH);
-  delay(1000);
-  
-  message="test123";
-
-  morseTalk(message);
-
-  digitalWrite(pttPin,LOW);
-  timeCounter=millis();
-  
-  
-  delay(500);
-  
-  */
+  Wire.begin();//IIC初始化
+  Serial.begin(9600);//设置波特率
+  Serial.println("DFRobot DFRduino GPS Shield v1.0");
+  Serial.println("$GPGGA statement information: ");
 }
-
-
-
-void loop(){
-   rec_init();
-   while (Wire.available()) {
-
-    // get the new byte:
-    char inChar = (char)Wire.read(); 
-    // add it to the inputString:
-    readLine += inChar;
-
-    if (inChar == '\n') {
-      stringComplete = true;
-    } 
-  }
-  //Serial.println("WireExit");
-  Wire.endTransmission();
-    //Serial.println("0");
-  
-  if (stringComplete) {
-      //Serial.println("1");
-    //Serial.println(readLine);
-    if(splitString(readLine,',',0)=="$GPGGA"){
-      time =splitString(readLine,',',1);
-      la =splitString(readLine,',',2);
-      ns =splitString(readLine,',',3);
-      lo =splitString(readLine,',',4);
-      ew =splitString(readLine,',',5);
-      al =splitString(readLine,',',9);
-    }
-    if(splitString(readLine,',',0)=="$GPGSV"){
-      sat = splitString(readLine,',',3);
-    }
-
-    fLa+=la.toFloat();
-    fLo +=lo.toFloat();
-    fAl +=al.toFloat();
-    floatCount ++;
-    
-    
-    if(morseTime){
-      //  Serial.println("2");
-      la=String(fLa/(float)floatCount);
-      lo=String(fLo/(float)floatCount);
-      al=String(fAl/(float)floatCount);
-      digitalWrite(pttPin,HIGH);
+void loop()
+{
+  while(1)
+  {
+    digitalWrite(pttPin,HIGH);
       delay(1000);
-/*
-
-      morseTalk("time:");
-      morseTalk(time);
-      morseTalk(",latitude:");
-      morseTalk(la);
-      morseTalk(",");
-      morseTalk(ns);
-      morseTalk(",longitude:");
-      morseTalk(lo);
-      morseTalk(",");
-      morseTalk(ew);
-      morseTalk(",altitude:");
-      morseTalk(al);
-      morseTalk(",satellites:");
-      morseTalk(sat);
-*/     
-     
-           message="time:"+time+"latitude:"+la+","+ns+"longitude:"+lo+","+ew+"altitude:"+al+"satellites:"+sat;
-     
-     morseTalk(message);
-
-     
-     
-//     morseTalk(message);
-
-        digitalWrite(pttPin,LOW);
-        morseTime =false;
-        timeCounter=millis();
-    }
-    
-    readLine ="";
-    stringComplete=false;
-
-    fLa= 0;
-    fLo=0;
-    fAl=0;
-    floatCount=0;
-   
-    
+    Serial.print("UTC:");
+    morseTalk('u');
+        morseTalk('t');
+            morseTalk('c');
+    morseTalk(':');
+    UTC();
+    morseTalk(',');
+    Serial.print("Lat:");
+    morseTalk('l');
+    morseTalk('a');
+    morseTalk('t');
+    morseTalk(':');
+    latitude();
+    morseTalk(',');
+    lat_dir();
+    morseTalk(',');
+    Serial.print("Lon:");
+    morseTalk('l');
+    morseTalk('o');
+    morseTalk('n');
+    morseTalk(':');
+    longitude();
+    morseTalk(',');
+    lon_dir();
+    morseTalk(',');
+    Serial.print("Alt:");
+    morseTalk('a');
+    morseTalk('l');
+    morseTalk('t');
+    morseTalk(';');
+    altitude();
+    morseTalk('m');
+    Serial.println(' ');
+    Serial.println(' ');
+            digitalWrite(pttPin,LOW);
   }
-   // Serial.println("3");
-//  if((millis()-timeCounter)>5*60*1000){
-  if((millis()-timeCounter)>100000){
-       morseTime=true; 
-  }
-  
-
-  digitalWrite(pttPin,HIGH);
-  // Serial.println("4");
-  delay(1000);
-  //Serial.println("singleline");
-  //morseTalk("singleline");//12chars
-//  delay(timeLetter);
-  digitalWrite(pttPin,LOW);
-  
-  delay(100);
-
 }
-
-String splitString(String s, char parser,int index){
-  String rs='\0';
-  int parserIndex = index;
-  int parserCnt=0;
-  int rFromIndex=0, rToIndex=-1;
-
-  while(index>=parserCnt){
-    rFromIndex = rToIndex+1;
-    rToIndex = s.indexOf(parser,rFromIndex);
-
-    if(index == parserCnt){
-      if(rToIndex == 0 || rToIndex == -1){
-        return '\0';
-      }
-      return s.substring(rFromIndex,rToIndex);
-    }
-    else{
-      parserCnt++;
-    }
-
-  }
-  return rs;
-}
-
 
 void toneOut (String character){
   //Serial.println("!TO");
@@ -220,16 +300,22 @@ void toneOut (String character){
 }
 
 
-void morseTalk(String str){
+
+
+void morseTalk(char c){
+  char tempS[1];
+ tempS[0]=c;
+morseTalk(tempS,1); 
+}
+
+
+void morseTalk(char * str,int n){
    //Serial.println("!MT");
    char tempStr;
-   str.toLowerCase();
-   Serial.print(str);
-   Serial.println(str.length());
-   for(int x =0,strlen =str.length();x<strlen;x++){
+   for(int x =0;x<n;x++){
      Serial.print(x);
      Serial.print(":");
-     tempStr = str.charAt(x);
+     tempStr =str[x];
      Serial.println(tempStr);
      if(tempStr=='a'){
          toneOut(".");
